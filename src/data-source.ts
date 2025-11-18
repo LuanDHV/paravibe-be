f4c9041b44ca03fbe845c9ae31107829b4fd564f
LuanDHV <luandhv1406@gmail.com>
2025-11-18 22:40:00 +0700

    setup docker

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,12 +4,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER || 'paravibe',
-  password: process.env.DB_PASS || 'mypass',
-  database: process.env.DB_NAME || 'paravibe_dev',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
-  synchronize: false, // production: false
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: false,
 });
